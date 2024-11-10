90461d1571d284c458540e31388e1f48bfd35591
Bawantha Mihiran Rajapaksha <bawwa.16@cse.mrt.ac.lk>
2024-11-10 17:22:27 +1100

    add tx

--- src/main/kotlin/com/example/routes/TransactionRoutes.kt
+++ src/main/kotlin/com/example/routes/TransactionRoutes.kt
@@ -21,7 +21,72 @@ fun Route.transactionRoutes(){
 
     route("/transactions") {
         // Route to add a transaction
+        post {
+            try {
+                val transaction = call.receive<Transaction>()
 
+                // Insert the new transaction into the MongoDB collection
+                val insertResult = transactionsCollection.insertOne(transaction)
+
+                if (insertResult.wasAcknowledged()) {
+                    call.respond(HttpStatusCode.Created, "Transaction added successfully.")
+                } else {
+                    call.respond(HttpStatusCode.InternalServerError, "Failed to add transaction.")
+                }
+            } catch (e: ContentTransformationException) {
+                call.respond(HttpStatusCode.BadRequest, "Invalid data format.")
+            }
+        }
         // Route to get all transactions
+        get {
+            val type = call.request.queryParameters["type"] ?: return@get call.respond(HttpStatusCode.BadRequest, "Missing Type")
+            try {
+                // Fetch all sales invoices
+                val transactions = when (type) {
+                    "payment" -> {
+                        // Fetch transactions with type "BPV" and "CPV"
+                        transactionsCollection.find(
+                            or(
+                                Transaction::type eq "BPV",
+                                Transaction::type eq "CPV"
+                            )
+                        ).toList()
+                    }
+                    "receipt" -> {
+                        // Fetch transactions with type "BRV" and "CRV"
+                        transactionsCollection.find(
+                            or(
+                                Transaction::type eq "BRV",
+                                Transaction::type eq "CRV"
+                            )
+                        ).toList()
+                    }
+                    else -> {
+                        // If no valid type parameter is provided, return a BadRequest
+                        return@get call.respond(HttpStatusCode.BadRequest, "Invalid Type")
+                    }
+                }
+                val responseList = mutableListOf<TransactionJson>()
+                for (transaction in transactions) {
+                    // Retrieve the partner for each invoice based on partnerId
+                    val partner = when (type) {
+                        "payment" -> {
+                            partnersCollection.findOneById(transaction.paymentTo)
+                        }
+                        "receipt" -> {
+                            partnersCollection.findOneById(transaction.receiptFrom)
+                        }
+                    }
+                    val jsonResponse = TransactionJson(partner, transaction)
+                    // Add each JsonResponse object to the list
+                    responseList.add(jsonResponse)
+                }
+                // Respond with the list of invoices with their corresponding partners
+                val jsonResponse = Json.encodeToString(responseList)
+                call.respond(HttpStatusCode.OK, jsonResponse)
+            } catch (e: Exception) {
+                call.respond(HttpStatusCode.InternalServerError, "Failed to retrieve ${type}s.")
+            }
+        }
     }
 }
