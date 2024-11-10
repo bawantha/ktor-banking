708566694e14d895e68a33f3806cc87abd3efde9
Bawantha Mihiran Rajapaksha <bawwa.16@cse.mrt.ac.lk>
2024-11-10 16:34:09 +1100

    add product

--- src/main/kotlin/com/example/routes/ProductRoutes.kt
+++ src/main/kotlin/com/example/routes/ProductRoutes.kt
@@ -1,6 +1,6 @@
 package com.exampleroutes
 
-import com.examplemodels.Product
+import com.example.models.Product
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.request.*
@@ -15,7 +15,22 @@ fun Route.productRoutes(){
 
     route("/products"){
         // Route to add a product
+        post {
+            try {
+                val product = call.receive<Product>()
 
+                // Insert the new product into the MongoDB collection
+                val insertResult = productsCollection.insertOne(product)
+
+                if (insertResult.wasAcknowledged()) {
+                    call.respond(HttpStatusCode.Created, "Product added successfully.")
+                } else {
+                    call.respond(HttpStatusCode.InternalServerError, "Failed to add product.")
+                }
+            } catch (e: ContentTransformationException) {
+                call.respond(HttpStatusCode.BadRequest, "Invalid product data format.")
+            }
+        }
         // Route to edit a product
 
         // Route to get product(s)
